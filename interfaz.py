cc50f247e0be3a7fd0e7e814e341a535adb09ea4
Amolin10 <molina102997@gmail.com>
2021-08-20 21:56:09 -0500

    Presentar el martes

--- interfaz.py
+++ interfaz.py
@@ -184,7 +184,7 @@ class Interfaz:
         icono = os.path.abspath(self.icon)
         self.root.iconbitmap(icono)
         ancho = 1200
-        alto = 650
+        alto = 600
         xVentana = self.root.winfo_screenwidth() // 2 - ancho // 2 #winfo da el tamaño de la pantalla en ancho y en alto
         yVentana = self.root.winfo_screenheight() // 2 - alto // 2
         posicion = str(ancho) + "x" + str(alto) + "+" + str(xVentana)+ "+" + str(yVentana)
@@ -210,8 +210,8 @@ class Interfaz:
         CBI.image = cbi
         CBI.grid(row=0, column=4, sticky='ewns')
 
-        titulo = Label(self.root, text="Insertar título", font=("Verdana", 22), background='whitesmoke').grid(row=0, column=1, columnspan=3, sticky='ewns')
-        texto = Label(self.root, text="Seleccionar una imagen, tipo y tiempo de animación para mostrar en la pantalla RGB.", font=("Verdana", 18), pady=30, background='white')
+        titulo = Label(self.root, text="Insertar título", font=("Verdana", 24), background='whitesmoke').grid(row=0, column=1, columnspan=3, sticky='ewns')
+        texto = Label(self.root, text="Seleccionar una imagen, tipo y tiempo de animación para mostrar en la pantalla RGB.", font=("Verdana", 20), pady=30, background='white')
         texto.grid(row=1, column=0, columnspan=5)
         
         ttk.Separator(self.root, orient=HORIZONTAL).grid(row=2, column=0, columnspan=5, sticky="ew")
@@ -220,26 +220,91 @@ class Interfaz:
 
         parametros = Frame(self.root, background='white')
         ########Poner una variable a los radiobutons##########################
-        Label(parametros, text="Selecciona el efecto para aplicar a la imagen", font=("Verdana", 12), pady=14, background='white').grid(column=0, row=0, columnspan=3)
-        Radiobutton(parametros, text="Instantaneo", font=("Verdana", 10), background='white').grid(column=0 , row=1,)
-        Radiobutton(parametros, text="De abajo a arriba", font=("Verdana", 10), background='white', padx=20).grid(column=1 , row=1, sticky="ew")
-        Radiobutton(parametros, text="Aleatorio", font=("Verdana", 10), background='white').grid(column=2, row=1, sticky="e")
-        Radiobutton(parametros, text="De derecha a izquierda", font=("Verdana", 10), background='white').grid(column=0, row=2, sticky="e")
-        Radiobutton(parametros, text="De arriba a abajo", font=("Verdana", 10), background='white').grid(column=2, row=2, sticky="w")
+        Label(parametros, text="Selecciona el efecto para aplicar a la imagen", font=("Verdana", 16), pady=14, background='white').grid(column=0, row=0, columnspan=3)
+        Radiobutton(parametros, text="Instantaneo", font=("Verdana", 12), background='white').grid(column=0 , row=1,)
+        Radiobutton(parametros, text="De abajo a arriba", font=("Verdana", 12), background='white', padx=20).grid(column=1 , row=1, sticky="ew")
+        Radiobutton(parametros, text="Aleatorio", font=("Verdana", 12), background='white').grid(column=2, row=1, sticky="e")
+        Radiobutton(parametros, text="De derecha a izquierda", font=("Verdana", 12), background='white').grid(column=0, row=2, sticky="e")
+        Radiobutton(parametros, text="De arriba a abajo", font=("Verdana", 12), background='white').grid(column=2, row=2, sticky="w")
 
         Label(parametros, text="", background="white").grid(column=0, row=3, columnspan=3)
 
         Label(parametros, text="Elija el tiempo: ", font=("Verdana", 12), pady=14, background='white', padx=10).grid(column=0, row=4, sticky="e")
         tiempo = Entry(parametros)
         tiempo.grid(column=1, row=4, sticky="w")
+        Label(parametros, text="segundos", font=("Verdana", 12), background="white").grid(column=1, row=4, sticky="e")
         parametros.grid(column=0, row=3, columnspan=2, sticky='n')
+        
 
 
         imagenFrame = Frame(self.root, background='white', pady=10)
-        Label(imagenFrame, text="Selecciona una imagen para\nmostrar en la pantalla RGB", font=("Verdana", 12), padx=10, pady=20, background='white').grid(row=0, column=0)
-        Button(imagenFrame, text="Cargar imagen", height=2, width=20, command=lambda:self.selecImagen(imagenFrame)).grid(row=0, column=1)
+        Label(imagenFrame, text="Selecciona una imagen para\nmostrar en la pantalla RGB", font=("Verdana", 14), padx=10, pady=20, background='white').grid(row=0, column=0)
+        imgImagen = Image.open("./iconos/imagen.png")
+        imgImagen = imgImagen.resize((20, 20))
+        imgImagen = ImageTk.PhotoImage(imgImagen)
+        botonCargar = Button(imagenFrame, text="Cargar imagen", image=imgImagen, compound="right", font=("Verdana", 12), command=lambda:self.selecImagen(imagenFrame))
+        botonCargar.grid(row=0, column=1)
+        botonCargar.image = imgImagen
         imagenFrame.grid(row=3, column=3, columnspan=2, sticky="n")
 
+        botonesFrame = Frame(self.root, background='white', pady=5)
+        
+        choice = OptionMenu(botonesFrame, "opción 1", "opción 2")
+        choice.config(width=15)
+        choice.grid(column=0, row=0, columnspan=2, padx=20, pady=25)
+
+        imgAñadir = Image.open("./iconos/añadir.png")
+        imgAñadir = imgAñadir.resize((20, 20))
+        imgAñadir = ImageTk.PhotoImage(imgAñadir)
+        botonAñadir = Button(botonesFrame, text="Añadir", image=imgAñadir, compound="right", font=("Verdana", 12))
+        botonAñadir.grid(column=0, row=1, padx=30)
+        botonAñadir.image = imgAñadir
+
+        imgEliminar = Image.open("./iconos/quit.jpg")
+        imgEliminar = imgEliminar.resize((20, 20))
+        imgEliminar = ImageTk.PhotoImage(imgEliminar)
+        botonEliminar = Button(botonesFrame, text="Eliminar", image=imgEliminar, compound="right", font=("Verdana", 12))
+        botonEliminar.grid(column=1, row=1, padx=30)
+        botonEliminar.image = imgEliminar
+
+        Label(botonesFrame, text="", padx=130, background="white").grid(row=0, column=2)
+
+        imgNuevo = Image.open("./iconos/nuevo.jpg")
+        imgNuevo = imgNuevo.resize((20, 20))
+        imgNuevo = ImageTk.PhotoImage(imgNuevo)
+        botonNuevo = Button(botonesFrame, text="Nuevo", image=imgNuevo, compound="right", font=("Verdana", 12))
+        botonNuevo.grid(column=3, row=0, rowspan=2, padx=35)
+        botonNuevo.image = imgNuevo
+
+        imgVis = Image.open("./iconos/visualizar.png")
+        imgVis = imgVis.resize((20, 20))
+        imgVis = ImageTk.PhotoImage(imgVis)
+        botonVis = Button(botonesFrame, text="Visualizar", image=imgVis, compound="right", font=("Verdana", 12))
+        botonVis.grid(column=4, row=0, rowspan=2, padx=35)
+        botonVis.image = imgVis
+
+        imgSave = Image.open("./iconos/save.ico")
+        imgSave = imgSave.resize((20, 20))
+        imgSave = ImageTk.PhotoImage(imgSave)
+        botonSave = Button(botonesFrame, text="Guardar", image=imgSave, compound="right", font=("Verdana", 12))
+        botonSave.grid(column=5, row=0, rowspan=2, padx=35)
+        botonSave.image = imgSave
+
+        botonesFrame.grid(column=0, row=4, columnspan=5)
+        botonesFrame.grid_rowconfigure(0,weight=1)
+        botonesFrame.grid_rowconfigure(1,weight=1)
+        botonesFrame.grid_columnconfigure(0,weight=1)
+        botonesFrame.grid_columnconfigure(1,weight=1)
+        botonesFrame.grid_columnconfigure(2,weight=1)
+        botonesFrame.grid_columnconfigure(3,weight=1)
+        botonesFrame.grid_columnconfigure(4,weight=1)
+        botonesFrame.grid_columnconfigure(5,weight=1)
+
+
+
+
+        
+        
         self.root.grid_columnconfigure(0,weight=1)
         self.root.grid_columnconfigure(1,weight=1)
         #self.root.grid_columnconfigure(2,weight=1)
@@ -280,9 +345,8 @@ class Interfaz:
 
         miMenu.add_cascade(label="Archivo", menu=archivo)
         miMenu.add_command(label="Ayuda", command=ayuda3)
-        miMenu.add_command(label="Información", command=informacion)
-
-
+        miMenu.add_command(label="Información", command=informacion)     
+    
     def destruir(self):
         self.root.destroy()
 
